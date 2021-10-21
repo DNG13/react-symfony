1a41a211584b7da18962b68ea9660ce664df734c
DNG13 <duka.natalja@gmail.com>
2021-10-21 18:42:15 +0300

    Passing Server Data to React Props

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -38,7 +38,7 @@ export default function RepLogs(props) {
     }
 
     return (
-        <div className="col-md-7">
+        <div>
             <h2>Lift History {heart} </h2>
             <input
                 type="range"
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -3,18 +3,11 @@ import {render} from "react-dom";
 import RepLogApp from './RepLog/RepLogApp';
 
 // console.log(<RepLogApp/>);
-const shouldShowHeart = true;
-const itemOptions = [
-    {id: 'cat', text: 'Cat'},
-    {id: 'fat_cat', text: 'Big Fat Cat'},
-    {id: 'laptop', text: 'My Laptop'},
-    {id: 'coffee_cup', text: 'Coffee Cup'},
-    {id: 'invalid_item', text: 'Dark matter'}
-]
+const shouldShowHeart = false;
 render(
     <RepLogApp
         withHeart={shouldShowHeart}
-        itemOptions={itemOptions}
+        {...window.REP_LOG_APP_PROPS}
     />,
     document.getElementById('lift-stuff-app')
 );
